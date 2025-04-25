7de3661e24c291d5321625b8b00818c800e5ef5f
Elias <johnpaulcauntoy@gmail.com>
2025-04-25 20:18:53 +0800

    first commit

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
 print("Hello! Im Prince Valdoz. This is my Final Activity in ITPT325")
 print("Hi! I'm Elias this is also my final activity")
+
+
+num = int(input("Enter a Number: "))
+
+print("Number: ", num)
